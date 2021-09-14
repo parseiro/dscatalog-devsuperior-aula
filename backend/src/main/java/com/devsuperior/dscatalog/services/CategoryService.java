9e572f6209266d3a3ce630f345cc06506a5e2f8c
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2021-09-14 18:18:22 -0300

    CategoryDTO and category fetching

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,13 +1,15 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repository.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,7 +17,17 @@ public class CategoryService {
     CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true) // tem que ser o import do Hibernate (não do Javax)
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
+    public List<CategoryDTO> findAll() {
+        return categoryRepository.findAll()
+                .parallelStream()
+                .map(CategoryDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Category entity = categoryRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Category not found"));
+
+        return new CategoryDTO(entity);
     }
 }
