32d3052c533ab1781a1ce7cbc840c8772fa08e89
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2021-09-14 18:17:54 -0300

    Exception handling for entity not found

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
@@ -0,0 +1,18 @@
+package com.devsuperior.dscatalog.resources.exceptions;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@NoArgsConstructor
+@Getter @Setter
+public class StandardError implements Serializable {
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
